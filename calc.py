37a7bb20ed595bb6dfc41e422269db898c4052bf
Tavin <tavinvishnu@outlook.com>
2025-09-16 20:45:03 +0200

    Update calc.py halfway

--- calc.py
+++ calc.py
@@ -1,17 +1,17 @@
-x = input("Gib ein erstes Zahl ein: ")
-y = input("Gib eine zweite Zahl ein: ")
-o = input("Gib einen Operator ein: ")
+x = float(input("Enter a first number: "))
+y = float(input("Enter an operator: "))
+o = input("Enter an Operator (+, -, /, *, %, //): ")
 
 
 if o == "+":
-    result = int(x) + int(y)
-    print("Die Antwort lautet: ", result)
+    result = x + y
+    print("The awnser is: ", result)
 elif o == "-":
-    result = int(x) - int(y)
-    print("Die Antwort lautet: ", result)
+    result = x - y
+    print("The awnser is: ", result)
 elif o == "*":
-    result = int(x) * int(y)
-    print("Die Antwort lautet: ", result)
+    result = x * y
+    print("The awnser is: ", result)
 elif o == "/":
     result = int(x) / int(y)
     print("Die Antwort lautet: ", result)
