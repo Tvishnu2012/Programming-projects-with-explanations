b0e805bce91213fee64b289204e60f9b7511eed3
Tavin <tavinvishnu@outlook.com>
2025-09-16 20:36:46 +0200

    Update 1-20RaisedTo2Or3.py
    Added the comments to help learners

--- 1-20RaisedTo2Or3.py
+++ 1-20RaisedTo2Or3.py
@@ -1,2 +1,5 @@
-for x in [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19 , 20]:
-    print(x, x**2, x**3)
+for x in [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,11 ,12 ,13 ,14 ,15 ,16 ,17 ,18 ,19 , 20]: #a loop that repeats the list called x till evry value is used
+    print(x, x**2, x**3) #print the value of the list x till every value is shown 
+#with the **2 or the **3 after the x so that the values oäf x will be shown raised to 2 and 3
+
+#try running this using the steps from the README.md
