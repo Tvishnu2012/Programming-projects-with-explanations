f045766764c09f95ef6dd920d560d015c82960f4
Tavin <tavinvishnu@outlook.com>
2025-09-17 16:35:51 +0200

    Update calculator.py and added comments

--- calculator.py
+++ calculator.py
@@ -1,14 +1,28 @@
-n1 = input("Enter a first number: ")
-n2 = input("Enter another number: ")
-addition = float(n1) + float(n2)
-print("Sum:", addition)
-multiplication = float(n1) * float(n2)
+n1 = input("Enter a first number: ") #asks for a first number using the input and asigning the variable n1 with the value of the input
+n2 = input("Enter another number: ") #same here but asks for a second number using a different text in the input code, and another name instead of n1 
+
+addition = float(n1) + float(n2) # makes a variable called addition that holds the value of the sum of the variables n1 and n2 both in float format.
+print("Sum:", addition) #prints Sum: and then the variable addition
+multiplication = float(n1) * float(n2) #same till the end using other variable names, operators and words when it prints the awnser
 print("Product:", multiplication)
 subtraction = float(n1) - float(n2)
 print("Difference:", subtraction)
-division =float(n1) / float(n2)
+division = float(n1) / float(n2)
 print("Quotient:", division)
-modulo =float(n1) % float(n2)
+modulo = float(n1) % float(n2)
 print("Rest:", modulo)
-divisionRoundedWithoutDecimals =float(n1) / float(n2)
+divisionRoundedWithoutDecimals = float(n1) / float(n2)
 print("Division without Decimals: ", divisionRoundedWithoutDecimals)
+
+# This is an example of the output which is also commented
+# Enter a first number: #for example 3 which you can type
+# Enter another number: #for example 6 which you can type as well
+# then it will print: the bit from above with the free line seperating it from the  input code but only the stuff inside the print with double quotes and then the variable for example
+# the modulo wich will then be printed under the division and like that in the order it is writen in code so
+
+# addition 
+# multiplication
+# subtraction
+# division
+# modulo
+# divisionRoundedWithoutDecimals
